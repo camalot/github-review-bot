559a01b69878672911a4fd2f4647b0531429c4f6
camalot <camalot@gmail.com>
2018-01-14 17:24:13 -0600

    im dumb and missed copy/pasta stuff

--- .deploy/shared.sh
+++ .deploy/shared.sh
@@ -4,17 +4,17 @@ __error() {
 	RED='\033[0;31m';
 	NC='\033[0m';
 	dt=$(date "+%F %T");
-	(>&2 echo "${RED}[$dt]\tERROR\t$(basename $0)\t${1:-"Unknown Error"}${NC}");
+	(>&2 echo -e "${RED}[$dt]\tERROR\t$(basename $0)\t${1:-"Unknown Error"}${NC}");
 	exit 9;
 }
 __warning() {
 	YELLOW='\033[0;33m';
 	NC='\033[0m';
 	dt=$(date "+%F %T");
-	(>&2 echo "${YELLOW}[$dt]\WARNING\t$(basename $0)\t${1:-"Unknown Warning"}${NC}");
+	(>&2 echo -e "${YELLOW}[$dt]\WARNING\t$(basename $0)\t${1:-"Unknown Warning"}${NC}");
 }
 __info() {
 	NC='\033[0m';
 	dt=$(date "+%F %T");
-	(>&2 echo "${NC}[$dt]\INFO\t$(basename $0)\t${1:-"Unknown Message"}${NC}");
+	(>&2 echo -e "${NC}[$dt]\INFO\t$(basename $0)\t${1:-"Unknown Message"}${NC}");
 }
