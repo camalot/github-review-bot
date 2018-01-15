6a5c5ca1887d3a8209d9db12553f8f8cc26f701b
camalot <camalot@gmail.com>
2018-01-14 21:19:37 -0600

    logging

--- app/services/github/github-api.js
+++ app/services/github/github-api.js
@@ -7,7 +7,6 @@ var github = new GitHubApi({
   debug: false,
   protocol: "https",
   host: "api.github.com", // should be api.github.com for GitHub
-  followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
   timeout: 8000,
 	version: '3.0.0',
 	headers: {
